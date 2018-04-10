746ff1a43c4fa02546d7f13297eb8360c43d1f0a
lexa <sokolov@ozero.net>
2018-04-10 18:47:43 +0300

    Program created

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -2,7 +2,17 @@ public class Person {
 
     HairColor hairColor = HairColor.BLACK;
 
-    public static void main(String[] args) {
+    public Person() {
+        int a = 5;
+    }
 
+    public static void main(String[] args) {
+        Person peter = new Person();
+        Person spiderMan = peter;
+
+        spiderMan.hairColor = HairColor.PINK;
+
+        System.out.println(peter.hairColor);
+        System.out.println(spiderMan.hairColor);
     }
 }
