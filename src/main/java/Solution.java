e92c7254683e01f7db865dfacf16302994fe893d
lexa <sokolov@ozero.net>
2018-04-10 19:20:15 +0300

    Input data done

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Solution {
     public static void main(String[] args) {
-
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scan.nextInt();
+        }
     }
 }
