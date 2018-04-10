6e0be5c2bb1518b1f9a2ad30ae569337f6b62757
lexa <sokolov@ozero.net>
2018-04-10 19:26:15 +0300

    Method declared

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 
 public class Solution {
+
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
@@ -8,5 +10,20 @@ public class Solution {
         for (int i = 0; i < arr.length; i++) {
             arr[i] = scan.nextInt();
         }
+    }
+
+    public void sort(int[] arr) {
+//        int numSwaps = 0;
+//
+//        for (int i = 0; i < arr.length; i++) {
+//            for (int j = 0; j < arr.length - 1; j++) {
+//                if (arr[j] > arr[j + 1]) {
+//                    numSwaps++;
+//                    int tmp = arr[j];
+//                    arr[j] = arr[j + 1];
+//                    arr[j + 1] = tmp;
+//                }
+//            }
+//        }
     }
 }
