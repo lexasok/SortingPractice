ae97ece35264f52bf4029ad612656feca7faacf0
lexa <sokolov@ozero.net>
2018-04-10 19:29:00 +0300

    Method renamed

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -12,8 +12,8 @@ public class Solution {
         }
     }
 
-    public void sort(int[] arr) {
-//        int numSwaps = 0;
+    public int sortGetNumOfSwaps(int[] arr) {
+        int numSwaps = 0;
 //
 //        for (int i = 0; i < arr.length; i++) {
 //            for (int j = 0; j < arr.length - 1; j++) {
@@ -25,5 +25,6 @@ public class Solution {
 //                }
 //            }
 //        }
+        return numSwaps;
     }
 }
