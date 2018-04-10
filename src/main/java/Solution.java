6ce75d452ce3f6745d993418c536e83efaf2b3fb
lexa <sokolov@ozero.net>
2018-04-10 19:32:53 +0300

    Method initialized, test passed

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -14,17 +14,17 @@ public class Solution {
 
     public int sortGetNumOfSwaps(int[] arr) {
         int numSwaps = 0;
-//
-//        for (int i = 0; i < arr.length; i++) {
-//            for (int j = 0; j < arr.length - 1; j++) {
-//                if (arr[j] > arr[j + 1]) {
-//                    numSwaps++;
-//                    int tmp = arr[j];
-//                    arr[j] = arr[j + 1];
-//                    arr[j + 1] = tmp;
-//                }
-//            }
-//        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    numSwaps++;
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
+            }
+        }
         return numSwaps;
     }
 }
